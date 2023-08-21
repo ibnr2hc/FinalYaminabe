869347e68c22d2cc239adf0c2e9311dda7469cbe
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-08-22 00:01:47 +0900

    結果一覧表示ページを作成した

---
+++ src/app/result/list/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { useStore } from "@/store/store"
+import { useRouter } from "next/navigation"
+
+
+export default function ResultPage() {
+    const router = useRouter()
+
+    const { decidedUserAndRoles } = useStore()
+    const userAndRoles = decidedUserAndRoles
+
+    return (
+        <div className="w-full flex justfiy-center items-center flex-col px-10 py-10">
+            <div className="rounded shadow-md ring-1 ring-gray-300 w-full">
+                <table className="min-w-full divide-y divide-gray-300">
+                    <thead>
+                        <tr>
+                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">ロール</th>
+                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">ポジション</th>
+                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">名前</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userAndRoles.map((userAndRole) => {
+                            return (
+                                <tr>
+                                    <td className='relative py-4 pl-4 pr-3 text-sm sm:pl-6'>{userAndRole.roleName}</td>
+                                    <td className='relative py-4 pl-4 pr-3 text-sm sm:pl-6'>{userAndRole.roleName}</td>
+                                    <td className='relative py-4 pl-4 pr-3 text-sm sm:pl-6'>{userAndRole.userName}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div className="mt-10">
+                <button
+                    className="border border-blue-700 text-blue-700 rounded-md shadow hover:bg-blue-700 hover:text-white px-12 py-2 mt-8"
+                    onClick={() => router.push("/")}
+                >
+                    入力画面へ戻る
+                </button>
+            </div>
+        </div>
+
+    )
+}
