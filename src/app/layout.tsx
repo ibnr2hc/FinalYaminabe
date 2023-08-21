2b4dd84209b24751b80aba0673984ba0b2b9466d
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-08-22 00:51:11 +0900

    微調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           </div>
 
           {/* Contents */}
-          <div className="flex items-center justify-center shadow-md container w-full rounded bg-slate-50 py-8">
+          <div className="flex items-center justify-center shadow-md container w-full rounded bg-slate-50 py-14">
             {children}
           </div>
 
