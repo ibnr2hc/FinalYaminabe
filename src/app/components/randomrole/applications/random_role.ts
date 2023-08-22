933d0c092018fadec98b9aebf09fa01d08976f5c
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-08-23 00:58:31 +0900

    4人用のRandomRoleを実装した

--- src/app/components/randomrole/applications/random_role.ts
+++ src/app/components/randomrole/applications/random_role.ts
@@ -2,6 +2,88 @@ import { DecidedUserAndRoleType, RoleType, UserType } from "@/store/store";
 import { ToastError } from "@/utils/toast";
 
 // TODO: ロジックが汚いのでリファクタリングする
+const randomRoleForFourPlayer = (users: UserType[]): DecidedUserAndRoleType[] => {
+    /* ランダムにロールを決める */
+    const MAX_RETRY_RANDOM_ROLE = 1000;  // ランダムロールの最大試行回数
+
+    // 各ロールごとの上限人数
+    const LIMIT_ROLE = {
+        TANK: 1,
+        HEALER: 1,
+        DPS: 2,
+    }
+
+    // 各ユーザーがselected=trueのロールの中からランダムで1つ選ぶ
+    for (let i = 0; i < MAX_RETRY_RANDOM_ROLE; i++) {
+        let userAndRoles: DecidedUserAndRoleType[] = []
+        let decidedRolesName: string[] = []
+        let decidedRoleCount = {  // 各ロールの振り分け済み人数
+            TANK: 0,
+            HEALER: 0,
+            DPS: 0,
+        }
+        users.forEach((user: UserType) => {
+            // 選択されているロールを取得する
+            const selectedRoles = getSelectedRole(user.roles)  // 選択されているロールを取得
+
+            // 既に上限を超えているロールを排除する
+            const candidateRoles = selectedRoles.filter((selectedRole: RoleType) => {
+                if (selectedRole.role == "TANK" && decidedRoleCount.TANK >= LIMIT_ROLE.TANK) {
+                    return false
+                } else if (selectedRole.role == "HEALER" && decidedRoleCount.HEALER >= LIMIT_ROLE.HEALER) {
+                    return false
+                } else if (selectedRole.role == "DPS" && decidedRoleCount.DPS >= LIMIT_ROLE.DPS) {
+                    return false
+                }
+                return true
+            })
+
+            // 候補のロールからランダムに1つ選ぶ
+            const randomRole = candidateRoles[Math.floor(Math.random() * candidateRoles.length)]
+
+            // TODO: 関数化する
+            // TODO: 処理をリファクタリングする
+            let role: string;
+            let sortPriority: number;
+            if (randomRole?.role == "TANK") {  // TankであればMTに振り分ける
+                role = "MT"
+                sortPriority = 1
+                decidedRoleCount.TANK += 1
+            } else if (randomRole?.role == "HEALER") {  // HEALERであればPH/BHを振り分ける
+                role = Math.random() < 0.5 ? "PH" : "BH"
+                sortPriority = 2
+                decidedRoleCount.HEALER += 1
+            } else {  // DPSはそのままロール(Melee/Range/Caster)で振り分ける
+                role = randomRole?.name
+                if (role == "Melee") {
+                    sortPriority = 3
+                } else if (role == "Range") {
+                    sortPriority = 4
+                } else {
+                    sortPriority = 5
+                }
+                decidedRoleCount.DPS += 1
+            }
+            decidedRolesName.push(role)
+
+            // 選出されたユーザーとロールを保存し、そのユーザーとロールを決定済みとして扱う
+            // この時、選択可能なロールがない場合は後続処理でエラーを出す
+            // TODO: ロジックが汚いので修正する。候補のロールがない場合のエラーを最適化する。
+            userAndRoles.push({userName: user?.name, roleName: role, sortPriority: sortPriority})  // 選ばれたユーザーとロール
+        })
+
+        // ロールが未定義のユーザーがいない場合は処理を終える
+        if (!existsUndefinedRole(userAndRoles)) {
+            return userAndRoles;
+        }
+    }
+
+    // ロールが未定義のユーザーがいる場合はアラートを表示する
+    ToastError("ユーザーとロールの数が一致しません")
+    throw Error("ユーザーとロールの数が一致しません")
+}
+
+// TODO: ロジックが汚いのでリファクタリングする
 const randomRoleForAllianceRaid = (users: UserType[]): DecidedUserAndRoleType[] => {
     /* ランダムにロールを決める */
     const MAX_RETRY_RANDOM_ROLE = 1000;  // ランダムロールの最大試行回数
@@ -236,5 +318,5 @@ function classNames(...classes: any[]) {
     return classes.filter(Boolean).join(' ')
 }
 
-export { classNames, existsUndefinedRole, getSelectedRole, randomRole, randomRoleForAllianceRaid, randomRoleForNormalRaid };
+export { classNames, existsUndefinedRole, getSelectedRole, randomRole, randomRoleForAllianceRaid, randomRoleForFourPlayer, randomRoleForNormalRaid };
 
