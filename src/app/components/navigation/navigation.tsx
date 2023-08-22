d1fdebcde942df4bfcf60423515f1e98c29a1264
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-08-22 22:19:01 +0900

    クリックをして戦闘コンテンツを選択できるようにした

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -11,28 +11,36 @@ export default function Navigation() {
         {
             name: "SAVAGE RAID",
             src: "/bg-savage-raid.jpeg",
-            disabled: false,
+            disabled: currentBattleContent == BattleContentEnum.SavageRaid,
             battleContent: BattleContentEnum.SavageRaid,
+            isImplemented: true,
         },
         {
             name: "NORMAL RAID",
             src: "/bg-normal-raid.jpeg",
-            disabled: true,
+            disabled: false,
             battleContent: BattleContentEnum.NormalRaid,
+            isImplemented: true,
         },
         {
             name: "ALLIANCE RAID",
             src: "/bg-alliance-raid.jpeg",
             disabled: true,
             battleContent: BattleContentEnum.AllianceRaid,
+            isImplemented: false,
         },
         {
             name: "4 PLAYER",
             src: "/bg-4player.jpeg",
             disabled: true,
             battleContent: BattleContentEnum.FourPlayer,
+            isImplemented: false,
         }
     ]
+
+    const changeBattleContent = (battleContent: BattleContentEnum) => {
+        setCurrentBattleContent(battleContent)
+    }
 
     return (
         <div className="w-full">
@@ -41,9 +49,10 @@ export default function Navigation() {
                     <div
                         key={content.name}
                         className={`${content.battleContent == currentBattleContent && "ring-4"}  flex justify-center border rounded-md bg-slate-50 shadow-md text-gray-700 font-semibold relative w-64 lg:w-30 h-20 md:h-32`}
+                        onClick={() => !content.disabled && changeBattleContent(content.battleContent)}
                     >
                         <Image className="shadow-md rounded-md" src={content.src} objectFit="cover" layout="fill" alt="Background Normal Raid" />
-                        <div className={`${content.disabled ? "bg-opacity-90":"bg-opacity-20"} relative rounded-md shadow-md flex justify-center items-center w-full bg-slate-700 text-white`}>{content.name}</div>
+                        <div className={`${content.isImplemented ? "bg-opacity-20":"bg-opacity-90"} relative rounded-md shadow-md flex justify-center items-center w-full bg-slate-700 text-white`}>{content.name}</div>
                     </div>
                 ))}
             </div>
